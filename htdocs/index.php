0a7ad46fc5e287644fe549ab749c7cfb22ffdcab
Curtis Wilcox <extra808@m.cognize.org>
2015-10-22 21:43:30 -0400

    add P3 site and repo links

--- htdocs/index.php
+++ htdocs/index.php
@@ -50,8 +50,8 @@ $lastMod = date ("F d Y", getlastmod());
         	<dt><a href="https://p2.dwa15.cognize.org">xkcd Password Generator</a></dt>
         	<dd><a href="https://github.com/extra808/dwa15-p2">Project 2 GitHub <abbr title="repository">repo</abbr></a></dd>
 
-        	<dt>Developer's Best Friend</dt>
-        	<dd>Project 3 GitHub <abbr title="repository">repo</abbr></dd>
+        	<dt><a href="https://p3.dwa15.cognize.org">Developer's Best Friend</a></dt>
+        	<dd><a href="https://github.com/extra808/dwa15-p3">Project 3 GitHub <abbr title="repository">repo</abbr></a></dd>
 
         	<dt>Final Project <abbr title="To Be Determined">TBD</abbr></dt>
         	<dd>Project 4 GitHub <abbr title="repository">repo</abbr></dd>
